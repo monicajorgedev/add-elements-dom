3327c1bb2ac3736dc58c268ac8a0790333269919
monicajorgedev <monicajorge.dev@gmail.com>
2024-06-06 13:37:22 +0200

    ejercicio resuelto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+ const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista')
+
+ agregar.addEventListener('click', function(){
+    let ciudad = prompt('Introduce un nombre de ciudad')
+    lista.insertAdjacentHTML('afterbegin', ('<li>' + ciudad + '</li>'))
+
+ })
+
+ 
