d6985d748e0caaa75d7429f103a5451613368bfa
monicajorgedev <monicajorge.dev@gmail.com>
2024-06-07 09:16:27 +0200

    agrego otra fomra

--- js/script.js
+++ js/script.js
@@ -3,9 +3,22 @@
  const lista = document.getElementById('lista')
 
  agregar.addEventListener('click', function(){
-    let ciudad = prompt('Introduce un nombre de ciudad')
-    lista.insertAdjacentHTML('afterbegin', ('<li>' + ciudad + '</li>'))
-
+    const ciudad = prompt('Introduce un nombre de ciudad')
+    lista.insertAdjacentHTML('beforeend', ('<li>' + ciudad + '</li>'))
+    
  })
 
  
+ 
+
+ /*const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista')
+
+ agregar.addEventListener('click', function(){
+
+    const ciudad = prompt('Introduce un nombre de ciudad')
+    const li = document.createElement('li')
+    li.textContent = ciudad;
+    lista.appendChild(li)
+    
+ }) */
